e9ac83c00c8c43234646a1adebeeb1b1592ba889
Diferfrei <diogofernandesfreitas@gmail.com>
2022-01-19 23:58:00 +0000

    Update ClientsController.js

--- Controllers/ClientsController.js
+++ Controllers/ClientsController.js
@@ -83,7 +83,8 @@ function addClient(req, res) {
 function deleteClient(req, res) {
     link = `https://api.moloni.pt/v1/customers/delete/?access_token=${access_token}`;
     const customer_id = req.customer_id;
-    request.delete({
+    console.log(customer_id);
+    request.post({
         url: link,
         form: {
             company_id: '205211',
