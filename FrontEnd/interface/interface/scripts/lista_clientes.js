e35ceb951b9ac1f7f6757d6b1a5f38d4e081d233
Diferfrei <diogofernandesfreitas@gmail.com>
2022-01-20 16:11:00 +0000

    Trying to make fetch work

---
+++ FrontEnd/interface/interface/scripts/lista_clientes.js
@@ -0,0 +1,41 @@
+window.onload = function () {
+    const SERVER_URL = "https://iaie.herokuapp.com/clients/all";
+    };
+    $(document).ready(function () {
+        fetch(SERVER_URL)
+            .then((response) => response.json())
+            .then(clients => {
+                var $table = $("#tabelaClients").DataTable({
+                    data: clients,
+                    columns: [
+                        { title: "#ID", data: "customer_id" },
+                        {
+                            title: "Nome",
+                            data: "name",
+                        },
+                        {
+                            title: "NIF",
+                            data: "vat",
+                        },
+                        {
+                            title: "Morada",
+                            data: "address",
+                        },
+                        {
+                            title: "Ações",
+                            data: null,
+                            render: function (value, cell, clients) {
+                                var action = `<a href="atividade-form.html">
+                                                            <button type="button" class="mb-2 mr-2 border-0 btn-lg btn-outline-light">
+                                                                <i class="pe-7s-look"> </i>
+                                                            </button>
+                                                        </a>
+                                                        `;
+                                return action;
+                            }
+                        }
+                    ]
+                });
+            })
+    });
+};
