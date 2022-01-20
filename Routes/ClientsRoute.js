f2eac8528a78ba4e55c9af5bc5b5388100cf1e6e
Diferfrei <diogofernandesfreitas@gmail.com>
2022-01-20 01:17:40 +0000

    weeee

--- Routes/ClientsRoute.js
+++ Routes/ClientsRoute.js
@@ -4,8 +4,7 @@ const controllerClients = require('../Controllers/ClientsController');
 
 // Routes
 router.get('/clients/all', controllerClients.listClients);
-//router.post('/clients/addClient/:customer_id', controllerClients.addClient);
-router.post('/clients/addClient', controllerClients.addClient);
+router.post('/clients/addClient/:customer_id', controllerClients.addClient);
 router.delete('/clients/deleteClient', controllerClients.deleteClient);
 
 module.exports = router;
